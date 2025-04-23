896223ef7d718ae2eee43e0df59187d425336c4b
Thaize Paulo <thaizepaulo@gmail.com>
2025-04-23 11:22:36 -0300

    codificação dos requisitos do desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite o número da conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Digite o número da agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Digite o nome do cliente:");
+        String cliente = scanner.nextLine();
+
+        System.out.print("Digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        //Exibir mensagem usando concat
+        String mensagem = "Olá ".concat(cliente)
+        .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+        .concat(agencia)
+        .concat(", conta ").concat(String.valueOf(numero))
+        .concat(" e seu saldo ").concat(String.valueOf(saldo))
+        .concat(" já está disponível para saque");
+        
+        System.out.println(mensagem);
+
+
     }
 }
